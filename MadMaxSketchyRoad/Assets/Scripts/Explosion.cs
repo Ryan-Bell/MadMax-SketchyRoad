d3967147d4a33f7a81b9e7e1d2fc5f8179fdc80b
Ryan Bell <42rbell@gmail.com>
2015-12-13 07:31:28 -0500

    Explosion emitter now affected by 3d perlin noise

--- MadMaxSketchyRoad/Assets/Scripts/Explosion.cs
+++ MadMaxSketchyRoad/Assets/Scripts/Explosion.cs
@@ -11,9 +11,40 @@ public class Explosion : MonoBehaviour {
     public int emitCount = 500;
 
     //The inverse factor for initial speed (ex. 0.5 will double init speed)
-    public float speedFactor = 0.3f;
+    public float speedFactor = 0.1f;
 
-	void Start () {
+    [Range(0f,1f)]
+    public float curlFactor = .4f;
+
+    public Vector3 offset;
+
+    public Vector3 rotation;
+
+    public float morphSpeed;
+
+    [Range(0f, 1f)]
+    public float strength = 1f;
+
+    public bool damping;
+
+    public float frequency = 1f;
+
+    [Range(1, 8)]
+    public int octaves = 1;
+
+    [Range(1f, 4f)]
+    public float lacunarity = 2f;
+
+    [Range(0f, 1f)]
+    public float persistence = 0.5f;
+
+    [Range(1, 3)]
+    public int dimensions = 3;
+
+    private float morphOffset;
+
+	void Start () 
+    {
 
         //get the particle system from the GameObject containing it
         system = systemHolder.GetComponent<ParticleSystem>();
@@ -27,11 +58,13 @@ public class Explosion : MonoBehaviour {
         //fill particle array with system's particles so they can be manipulated
         system.GetParticles(particleArray);
         
+
         float offset = 2.0f / emitCount;
         float increment = Mathf.PI * (3.0f - Mathf.Sqrt(5.0f));
         float x, y, z, r;
 
-        //loop set the initial velocity of each of the particles in radial pattern using fibonnacci spiral
+        //set the initial velocity and position of each of the particles in radial pattern using fibonnacci spiral
+        //the fibonnacci spiral method prevents bunching of particles near the poles of the sphere (lat and long method)
         for (float i = 0; i < system.particleCount; i++)
         {
             y = ((i * offset) - 1) + (offset / 2.0f);
@@ -44,7 +77,8 @@ public class Explosion : MonoBehaviour {
             z = Mathf.Sin(phi) * r;
 
             //set particle position and velocity
-            particleArray[(int)i].position = new Vector3(x, y, z);
+            particleArray[(int)i].position = new Vector3(0, 0, 0);
+
             particleArray[(int)i].velocity = new Vector3(x / speedFactor, y / speedFactor, z / speedFactor);
 
         }
@@ -52,13 +86,51 @@ public class Explosion : MonoBehaviour {
         system.SetParticles(particleArray, system.particleCount);
 	}
 	
-	void Update () {
-        //there should be a factor so that the particles slow down (maybe maxspeed?)
-            //the slow down should be a steep falloff rather than linear
+	void LateUpdate () 
+    {
         //the color and dissapation can be set with the particle system itself
 
 	    //get all the particles
-        //loop through and change velocity based on perlin noise field
+        system.GetParticles(particleArray);
+
+        //calculate the particle's movement based on 3d perlin flow field
+        PositionParticles();
+
         //set particles
+        system.SetParticles(particleArray, system.particleCount);
 	}
+
+    private void PositionParticles()
+    {
+        Quaternion q = Quaternion.Euler(rotation);
+        Quaternion qInv = Quaternion.Inverse(q);
+        NoiseMethod method = Noise.methods[0][0];
+        float amplitude = damping ? strength / frequency : strength;
+        morphOffset += Time.deltaTime * morphSpeed;
+        if (morphOffset > 256f)
+        {
+            morphOffset -= 256f;
+        }
+        for (int i = 0; i < particleArray.Length; i++)
+        {
+            Vector3 position = particleArray[i].position;
+            Vector3 point = q * new Vector3(position.z, position.y, position.x + morphOffset) + offset;
+            NoiseSample sampleX = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+            sampleX *= amplitude;
+            sampleX.derivative = qInv * sampleX.derivative;
+            point = q * new Vector3(position.x + 100f, position.z, position.y + morphOffset) + offset;
+            NoiseSample sampleY = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+            sampleY *= amplitude;
+            sampleY.derivative = qInv * sampleY.derivative;
+            point = q * new Vector3(position.y, position.x + 100f, position.z + morphOffset) + offset;
+            NoiseSample sampleZ = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+            sampleZ *= amplitude;
+            sampleZ.derivative = qInv * sampleZ.derivative;
+            Vector3 curl;
+            curl.x = sampleZ.derivative.x - sampleY.derivative.y;
+            curl.y = sampleX.derivative.x - sampleZ.derivative.y + (1f / (1f + position.y));
+            curl.z = sampleY.derivative.x - sampleX.derivative.y;
+            particleArray[i].velocity = particleArray[i].velocity * (1f - curlFactor) + curl * curlFactor;
+        }
+    }
 }
